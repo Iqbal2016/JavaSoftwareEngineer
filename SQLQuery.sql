9ca83182a638313e9784307d770a60068f3be7f2
Mohd Iqbal Hossain <iqbal.cse2016@gmail.com>
2021-12-04 14:55:17 +0600

    add SQL query file & Documantation file

---
+++ SQLQuery.sql
@@ -0,0 +1,35 @@
+------------------------------------Question No: 1.1 -----------------------------------
+-------------------------------------Used DB:PostgresSQL--------------------------------
+-------------------------------------Create Account Table ------------------------------
+
+CREATE TABLE Account
+(
+    id bigint NOT NULL,
+    name character varying(100),
+    address character varying(255),
+    account_number character varying(16),
+    balance double precision DEFAULT 0  NOT NULL,
+    status character varying(50),
+    PRIMARY KEY (id)
+);
+
+-------------------------------------Insert value Account Table ------------------------------
+
+INSERT INTO Account(id, name, address,  account_number, balance, status) VALUES(1, 'Mohammad', 'Madaripur' , '1241009211439' , 5000, 'Active');
+INSERT INTO Account(id, name, address,  account_number, balance, status) VALUES(2, 'Hossain', 'Rampura' , '32341200923487' , 5000, 'Active');
+INSERT INTO Account(id, name, address,  account_number, balance, status) VALUES(3, 'Iqbal', 'Dhaka' , '123456789123456' , 5000, 'Active');
+
+
+-------------------------------------Create Transaction Table ------------------------------
+
+CREATE TABLE Transaction
+(
+request_id character varying(50) NOT NULL, 
+transaction_time timestamp without time zone,
+requester character varying(255),
+transaction_type character varying(255),
+source_account_number character varying(255),
+amount double precision DEFAULT 0  NOT NULL,
+destination_account_number character varying(255),
+note character varying(255)
+);
